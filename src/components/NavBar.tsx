f5fa768cd048c4c01160af0627569f6aa12eafc7
Darcy <drc@cmail.com>
2025-02-04 08:44:12 +0200

    add flyionui and change navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,23 +2,42 @@ import Link from 'next/link'
 
 export function NavBar({ isAdmin, isAuthed }: { isAdmin: boolean, isAuthed: boolean }) {
     return (
-        <nav className="flex gap-3 justify-center">
-            {isAuthed ?
-                <>
-                    <Link href="/posts">Posts</Link>
-                    <Link href="/profile">Profile</Link>
-                    {isAdmin && (
-                        <>
-                            <Link href="/dashboard">Dashboard</Link>
-                        </>
-                    )}
-                </>
-                :
-                <>
-                    <Link href="/login">Login</Link>
-                    <Link href="/signup">Signup</Link>
-                </>
-            }
+        <nav className="navbar rounded-box shadow">
+            <div className="w-full md:flex md:items-center md:gap-2">
+                <div className="flex items-center justify-between">
+                    <div className="navbar-start items-center justify-between max-md:w-full">
+                        <a className="link text-base-content link-neutral text-xl font-semibold no-underline" href="#">LMaps</a>
+                        <div className="md:hidden">
+                            <button type="button" className="collapse-toggle btn btn-outline btn-secondary btn-sm btn-square" data-collapse="#default-navbar-collapse" aria-controls="default-navbar-collapse" aria-label="Toggle navigation" >
+                                <span className="icon-[tabler--menu-2] collapse-open:hidden size-4"></span>
+                                <span className="icon-[tabler--x] collapse-open:block hidden size-4"></span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div id="default-navbar-collapse" className="md:navbar-end collapse hidden grow basis-full overflow-hidden transition-[height] duration-300 max-md:w-full" >
+                    <ul className="menu md:menu-horizontal gap-2 p-0 text-base max-md:mt-2">
+
+                        {isAuthed ?
+                            <>
+                                <li><Link href="/posts">Posts</Link></li>
+                                <li><Link href="/profile">Profile</Link></li>
+                                {isAdmin && (
+                                    <li>
+                                        <Link href="/dashboard">Dashboard</Link>
+                                    </li>
+                                )}
+                            </>
+                            :
+                            <>
+                                <li><Link href="/posts">Posts</Link></li>
+                                <li><Link href="/profile">Login</Link></li>
+                            </>
+
+                        }
+                    </ul>
+                </div>
+            </div>
         </nav>
     )
 }
