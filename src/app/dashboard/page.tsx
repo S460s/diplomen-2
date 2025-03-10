19e991d16796a9f1309b37fc7b6743e22d817ede
Darcy <drc@cmail.com>
2025-03-10 09:29:40 +0200

    bs styles

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,21 @@
 import Link from "next/link";
 
 export default async function Page() {
-    return <div>
-        <h1>Admin stuff here</h1>
-        <Link href={'/dashboard/users'}>Manage users</Link>
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="card md:w-1/2 p-12">
+        <h1>Admin Dashboard</h1>
 
+        <ul className="list-inside list-disc">
+          <li className="mb-2">
+            <Link href={"/dashboard/users"}>Manage maps</Link>
+          </li>
+
+          <li className="mb-2">
+            <Link href={"/dashboard/maps"}>Manage users</Link>
+          </li>
+        </ul>
+      </div>
     </div>
+  );
 }
--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,22 +1,26 @@
-import prisma from "@/lib/prisma"
-import Link from 'next/link'
+import prisma from "@/lib/prisma";
+import Link from "next/link";
 
 export default async function Page() {
-    const users = await prisma.user.findMany();
+  const users = await prisma.user.findMany();
 
-    return <div>
-        <h1>Manage users</h1>
-        <br />
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="card md:w-1/2 p-12">
+        <h1>Manage Users</h1>
 
         <ul>
-            {users.map((user) => (
-                <ul key={user.id}>
-                    <li>Name: {user.name}</li>
-                    <li>Email: {user.email}</li>
-                    <Link href={`/dashboard/users/${user.id}`}>Edit</Link>
-                    <br /><br />
-                </ul>
-            ))}
+          {users.map((user) => (
+            <ul key={user.id} className="mb-4">
+              <li>Name: {user.name}</li>
+              <li>Email: {user.email}</li>
+              <button className="btn btn-primary">
+                <Link href={`/dashboard/users/${user.id}`}>Edit</Link>
+              </button>
+            </ul>
+          ))}
         </ul>
+      </div>
     </div>
+  );
 }
